d4a7c4515d4e327f96961e06cb95d9c9c7659968
KarthikReddyVemula1103 <vemulakarthik2001@gmail.com>
2026-08-26 20:22:15 +0530

    Add files via upload

---
+++ Day1.js
@@ -0,0 +1,20 @@
+
+let studentName = "Karthik";
+let age = 24;
+let mark = 85;
+
+document.write("Student Name: " + studentName + "<br>");
+document.write("Age: " + age + "<br>");
+document.write("Mark: " + mark + "<br>");
+
+let name = prompt("Enter your name");
+alert(name);
+
+let userAge = prompt("Enter your age");
+console.log(userAge);
+
+let answer = confirm("Do you want to continue?");
+console.log(answer);
+
+let userName = prompt("Enter your name");
+document.writeln(userName);
